43291c77051ab3d78b497878d074982d440df96f
Syrinca <165695271+Syrinca@users.noreply.github.com>
2024-04-05 05:07:24 +0800

    Add files via upload

---
+++ NaA.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Hello, " + name + "!");
+        Console.WriteLine("You are " + age + " years old.");
+    }
+}
